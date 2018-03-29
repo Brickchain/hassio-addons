f236ac4e0ed4fd8807aa3375f7630dacbb1d1924
Lars Larsson <lars.la@gmail.com>
2018-03-29 11:48:00 +0200

    ignore empty remote config

--- brickchain-proxy/run.sh
+++ brickchain-proxy/run.sh
@@ -7,7 +7,7 @@ CONFIG_PATH=/data/options.json
 export SECRET=$(jq --raw-output ".secret" $CONFIG_PATH)
 
 CONFIG_REMOTE=$(jq --raw-output ".remote" $CONFIG_PATH)
-if [ -n "$CONFIG_REMOTE" ]; then
+if [ -n "$CONFIG_REMOTE" ] && [ "$CONFIG_REMOTE" != "" ]; then
     export REMOTE=$CONFIG_REMOTE
 fi
 
